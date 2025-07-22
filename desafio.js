9fd2628920994dfb5afe0ae2a1bfe07ceb1a05da
raistergt <raistergt@gmail.com>
2025-07-22 20:51:08 -0300

    desafio

--- desafio.js
+++ desafio.js
@@ -1,1 +1,38 @@
-console.log("teste")
+// Desafio Felipao
+
+let nomeHeroi = ("Felipão")
+let xP = 50000
+
+if(xP <= 1000){
+    console.log("O herói de nome " + nomeHeroi + " está no nível de ferro")
+}
+
+if (xP>=1001 && xP<=2000){
+    console.log("O herói de nome  " + nomeHeroi + " está no nível de Bronze")
+}
+
+if (xP>=2001 && xP<=5000){
+    console.log("O herói de nome  " + nomeHeroi + " está no nível de Prata")
+}
+
+if (xP>=5001 && xP<=7000){
+    console.log("O herói de nome  " + nomeHeroi + " está no nível de Ouro")
+}
+
+if (xP>=7001 && xP<=8000){
+    console.log("O herói de nome  " + nomeHeroi + " está no nível de Platina")
+}
+
+if (xP>=8001 && xP<=9000){
+    console.log("o herói de nome  " + nomeHeroi + " está no nível de Ascendente")
+
+}
+
+if (xP>=9001 && xP<=10000){
+    console.log("O herói de nome  " + nomeHeroi + " está no nível de Imortal")
+
+}
+
+if (xP>=10001){
+    console.log("O herói de nome  " + nomeHeroi + " está no nível de Radiante")
+}
